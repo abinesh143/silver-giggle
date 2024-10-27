c639b3e1b4c5b1d87d60fe307d24d6a62a32163e
Abinesh <abinesh@Abineshs-MacBook-Pro.local>
2024-10-27 21:28:15 +0530

    gif page

--- src/pages/cartoon/index.jsx
+++ src/pages/cartoon/index.jsx
@@ -1,5 +1,6 @@
 'use client'
 import Image from "next/image"
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
@@ -24,9 +25,15 @@ const MyCartoonSticker = (props) => {
                 <div>
                     <Image src={data ? `${url}${data}` : '/sticker/template-1.gif'} width={240} height={240} alt={'photo-sticker'} className="w-full h-full rounded-lg" />
                 </div>
-
             </div>
+            <Link
+                href={'https://www.freeappmaker.pro/sticker-maker'}
+                className="bg-yellow-400 text-center text-white text-xs sm:text-base lg:text-sm xl:text-base font-medium rounded-lg sm:rounded-xl hover:bg-opacity-80  focus:ring-4 focus:outline-none px-8 py-3 sm:px-14 sm:py-3 lg:px-10 lg:py-2 2xl:px-16 2xl:py-4 max-lg:w-full"
+            >
+                Make your Gif
+            </Link>
         </div>
+
     </main>
 }
 
