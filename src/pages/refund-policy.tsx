b1a176f673c87416a3a47a9560acd5449b8383ee
Abinesh <abinesh@Abineshs-MacBook-Pro.local>
2024-12-08 18:48:32 +0530

    refund-policy

--- src/pages/refund-policy.tsx
+++ src/pages/refund-policy.tsx
@@ -26,153 +26,54 @@ const RefundPolicy = () => {
               <div className=" px-4 sm:px-8 lg:px-6 xl:px-8 pt-4 sm:pt-8 lg:pt-6 xl:pt-8 lg:pb-20 max-lg:text-justify relative">
                 <div className="pb-16 lg:pb-0">
                   <p>
-                    <span className="font-semibold">Free App Maker Pro AI</span>{" "}
-                    (“Free App Maker Pro AI”) owns and operates this
-                    freeappmaker.pro website business. All references to “we”,
-                    “us”, “apps”, this “website” or this “site” shall be
-                    construed to mean Free App Maker Pro AI. We understand that
-                    visitors are concerned about the privacy of information. The
-                    following describes our privacy policy regarding
-                    information, including Personal Information, that we collect
-                    through this website and generated apps. By using this
-                    website you are accepting the practices described in this
-                    Privacy Policy.
+                    This cancellation policy outlines how you can cancel or seek
+                    a refund for a product/service that you have purchased
+                    through the Platform. Under this policy:
                   </p>
-
-                  <h2 className="font-semibold my-4">
-                    HOW WE MODIFY THIS PRIVACY POLICY
-                  </h2>
-                  <p>
-                    We reserve the right to modify this Privacy Policy at any
-                    time, and without prior notice, by posting an amended
-                    Privacy Policy that is always accessible by clicking on the
-                    “Privacy Policy” link on this site&apos;s home page. Your
-                    continued use of this site indicates your acceptance of the
-                    amended Privacy Policy. You should check the Privacy Policy
-                    periodically for modifications.
-                  </p>
-
-                  <h2 className="font-semibold my-4">
-                    THE TYPES OF INFORMATION WE COLLECT
-                  </h2>
-                  <p>
-                    Personal Information. We collect Personal Information that
-                    you provide to us and that may be used to identify you
-                    (“Personal Information”). Personal Information that we
-                    collect may vary with each separate purpose for which you
-                    provide it, and it may include one or more of the following
-                    categories: name, physical address, an email address, phone
-                    number, and credit card information including credit card
-                    number, expiration date, and billing address.
-                  </p>
-
-                  <p>
-                    Non-Personal Information. We reserve the right to collect
-                    anonymous information such as browser type, the URL of the
-                    previous website you visited, your computer&apos;s operating
-                    system and Internet protocol (IP) Address, Internet domain
-                    and host name, your Internet Service provider, your
-                    clickstream data, and the dates and times that you may
-                    access this site and specific pages (“Non-Personal
-                    Information”). Non-Personal Information is essentially
-                    anonymous when collected; however, it&apos;s possible that
-                    Non-Personal Information could be used to identify you.
-                  </p>
-
-                  <h2 className="font-semibold my-4">
-                    HOW WE USE YOUR INFORMATION
-                  </h2>
-                  <p>
-                    We use your Personal Information for the performance of the
-                    services or transaction for which it was given, our private,
-                    internal reporting for this site, and security assessments
-                    for this site.
-                  </p>
-                  <p>
-                    We reserve the right to make full use of Non-Personal
-                    Information. For example, we may use Non-Personal
-                    Information to provide better service to site visitors,
-                    customize the site based on your preferences, compile and
-                    analyze statistics and trends about the use of this site,
-                    and otherwise administer and improve this site for your use.
-                  </p>
-
-                  <h2 className="font-semibold my-4">
-                    HOW WE DISCLOSE YOUR INFORMATION
-                  </h2>
-                  <p>
-                    General Disclosure Policy. Our general policy is that we
-                    will not share, sell, or rent your Personal Information to
-                    others. The only exceptions to this general policy: (i) are
-                    described in the subsections below, and (ii) if you
-                    explicitly approve through our site. We reserve the right to
-                    disclose Non-Personal Information without restriction.
-                  </p>
-
-                  <h2 className="font-semibold my-4">DATA SECURITY</h2>
-                  <p>
-                    We follow reasonable and appropriate industry standards to
-                    protect your Personal Information and data. Unfortunately,
-                    no data transmission over the Internet or method of data
-                    storage can be guaranteed 100% secure. Therefore, while we
-                    strive to protect your Personal Information by following
-                    generally accepted industry standards, we cannot ensure or
-                    warrant the absolute security of any information you
-                    transmit to us or archive at this site.
-                  </p>
-
-                  <h2 className="font-semibold my-4">
-                    HOW TO DELETE YOUR PERSONAL INFORMATION
-                  </h2>
-
-                  <p>
-                    Send request to{" "}
-                    <a href="mailto:smileysoftwares@gmail.com">
-                      smileysoftwares@gmail.com
-                    </a>
-                  </p>
-
-                  <h2 className="font-semibold my-4">
-                    LINKS TO OTHER SITES; ONLINE ADS
-                  </h2>
-                  <p>
-                    This site may contain links to other websites with whom we
-                    have a business relationship. These links may include online
-                    advertisements that we deem to be appropriate. Any
-                    information that you provide in the process of registration
-                    or purchase will be transferred to these sites. We have no
-                    responsibility or liability for the policies and practices
-                    of these sites. You should be careful to review any privacy
-                    policies posted on any of these sites before providing
-                    information to them.
-                  </p>
-
-                  <h2 className="font-semibold my-4">
-                    CHILDREN&apos;S ONLINE POLICY
-                  </h2>
-                  <p>
-                    We are committed to preserving online privacy for all of its
-                    website visitors, including children. This site is a general
-                    audience site. Consistent with the Children&apos;s Online
-                    Privacy Protection Act (COPPA), we will not knowingly
-                    collect any information from, or sell to, children under the
-                    age of 13. If you are a parent or guardian who has
-                    discovered that your child under the age of 13 has submitted
-                    his or her personally identifiable information without your
-                    permission or consent, we will remove the information from
-                    our active list, at your request. To request the removal of
-                    your child&apos;s information, please contact our site as
-                    provided below under “Contact Us”, and be sure to include in
-                    your message the same login information that your child
-                    submitted.
-                  </p>
-
-                  <h2 className="font-semibold my-4">CONTACT US</h2>
-                  <p>
-                    If you have any questions regarding this Privacy Policy,
-                    please contact the owner and operator of this website
-                    business at:
-                  </p>
+                  <ul>
+                    <li>
+                      Cancellations will only be considered if the request is
+                      made within <strong>Free App Maker AI</strong> days of placing the
+                      order. However, cancellation requests may not be
+                      entertained if the orders have been communicated to such
+                      sellers/merchant(s) listed on the Platform and they have
+                      initiated the process of shipping them, or the product is
+                      out for delivery. In such an event, you may choose to
+                      reject the product at the doorstep.
+                    </li>
+                    <li>
+                      <strong>Free App Maker AI</strong> does not accept cancellation requests
+                      for perishable items like flowers, eatables, etc. However,
+                      the refund/replacement can be made if the user establishes
+                      that the quality of the product delivered is not good.
+                    </li>
+                    <li>
+                      In case of receipt of damaged or defective items, please
+                      report to our customer service team. The request will be
+                      entertained once the seller/merchant listed on the
+                      Platform has checked and determined the same at its own
+                      end. This should be reported within <strong>Free App Maker AI</strong>{" "}
+                      days of receipt of the products.
+                    </li>
+                    <li>
+                      If you feel that the product received is not as shown on
+                      the site or as per your expectations, you must bring it to
+                      the notice of our customer service within{" "}
+                      <strong>Free App Maker AI</strong> days of receiving the product. The
+                      customer service team, after looking into your complaint,
+                      will take an appropriate decision.
+                    </li>
+                    <li>
+                      In case of complaints regarding products that come with a
+                      warranty from the manufacturers, please refer the issue to
+                      them.
+                    </li>
+                    <li>
+                      In case of any refunds approved by <strong>Free App Maker AI</strong>,
+                      it will take <strong>Free App Maker AI</strong> days for the refund to
+                      be processed to you.
+                    </li>
+                  </ul>
                   <p>
                     Email:{" "}
                     <a href="mailto:smileysoftwares@gmail.com">
